2061ceedc08c6b97ba777fb06644ce1a82422417
vicente_ogalde <vicenteogalde@gmail.com>
2023-05-10 10:02:10 -0300

    commit 39

--- 04-react-intro/counter-app-vite/src/FirstApp.jsx
+++ 04-react-intro/counter-app-vite/src/FirstApp.jsx
@@ -1,3 +1,12 @@
+const newMessage = {m1:"hello",m2:"hello2"};
+
+const getFirstMessage=(message)=>message.m2;
+
 export function FirstApp(){
-    return <h2>First App</h2>
+
+
+    return <>
+    <h2>First App {getFirstMessage(newMessage)}</h2>
+    <h3>---------</h3>
+    </>
 }
