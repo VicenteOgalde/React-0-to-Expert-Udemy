90acf09c7235a57e78c21853806bb3a5838e37b5
vicente_ogalde <vicenteogalde@gmail.com>
2023-05-10 21:08:43 -0300

    commit 47

--- 04-react-intro/counter-app-vite/src/CounterApp.jsx
+++ 04-react-intro/counter-app-vite/src/CounterApp.jsx
@@ -11,10 +11,18 @@ export const CounterApp=({ counter })=>{
     const handleAdd=()=>{
         setValue((c)=>c+1);
     }
+    const handleSubtract=()=>{
+        setValue((c)=>c-1);
+    }
+    const handleReset=()=>{
+        setValue(counter);
+    }
     return <>
     <h1>CounterApp</h1>
     <h2>{ value }</h2>
     <button onClick={handleAdd}>+1</button>
+    <button onClick={handleSubtract}>-1</button>
+    <button onClick={handleReset}>reset</button>
     </>
 }
 
