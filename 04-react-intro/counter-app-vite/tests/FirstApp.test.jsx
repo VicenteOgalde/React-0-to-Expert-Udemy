3adaf053559c24698e21854fe2ef229bbf415978
vicente_ogalde <vicenteogalde@gmail.com>
2023-05-14 20:27:44 -0400

    commit 64

--- 04-react-intro/counter-app-vite/tests/FirstApp.test.jsx
+++ 04-react-intro/counter-app-vite/tests/FirstApp.test.jsx
@@ -1,11 +1,23 @@
 /** * @jest-environment jsdom */
-import { render } from '@testing-library/react';
+import { getByText, render } from '@testing-library/react';
 import { FirstApp } from './../src/FirstApp';
 
 describe('test FirsApp', () => {
 
      test('should make match with the snapshot', () => {
 
-        render(<FirstApp title='nice App' subTitle='very nice App'></FirstApp>)
-         }) 
-    })
+        const {container}=render(<FirstApp title='nice App' subTitle='very nice App'></FirstApp>)
+
+        expect(container).toMatchSnapshot();
+
+         });
+
+         test('should show the title in a H1', () => {
+
+          const title ='good morning'
+          const {container, getByText}=render(<FirstApp title={title} subTitle='very nice App'></FirstApp>)
+  
+          expect(getByText(title) ).toBeTruthy();
+  
+           });  
+    });
